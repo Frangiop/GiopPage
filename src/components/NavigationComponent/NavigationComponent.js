755b0f1d0190d2e3413978fc0924d31636dc60b1
Frangiop <cybergiunti@gmail.com>
2023-11-01 18:27:31 +0100

    Adding css

--- src/components/NavigationComponent/NavigationComponent.js
+++ src/components/NavigationComponent/NavigationComponent.js
@@ -1,16 +1,49 @@
-import React from 'react';
+
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-
+import React, { useState, useEffect } from 'react';
+import './NavigationComponent.css'; 
 
 const NavigationComponent = () => { 
-return (
-<div>
-<Link to='/artist'> Artist</Link> 
-<Link to='/collections'>Collections</Link>  
-<Link to='/digital-art'>Digital Art</Link>
 
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+  // Determina la cantidad de píxeles que el usuario debe desplazarse hacia abajo antes de ocultar la barra de navegación
+  const scrollThreshold = 170;
+
+  const visible = prevScrollPos > currentScrollPos || currentScrollPos < scrollThreshold;
+
+  setVisible(visible);
+  setPrevScrollPos(currentScrollPos);
+};
+window.addEventListener('scroll', handleScroll);
+
+
+return () => {
+  window.removeEventListener('scroll', handleScroll);
+};
+}, [prevScrollPos]);
+
+// Ocultar la barra de navegación cuando scrollingDown es true
+const navClass = visible ? 'navbar' : 'navbar hidden';
+return (
+  <div className={navClass}>
+  <div className="logo">
+  <div>Giop</div>
   </div>
+      <div className="line"></div>
+      <div className="nav-links">
+   
+<Link to='/artist' className="nav-link" > Artist</Link> 
+<Link to='/collections' className="nav-link" >Collections</Link>  
+<Link to='/digital-art'className="nav-link" >Digital Art</Link>
+</div>
+    </div>
 )
  };
 
