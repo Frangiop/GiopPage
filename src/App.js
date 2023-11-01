bd99fe2c9284fbbc6c79d11b47e7909a59255d58
Frangiop <cybergiunti@gmail.com>
2023-11-01 15:56:32 +0100

    adding component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    hola !"·!"
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavigationComponent/NavigationComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const NavigationComponent = () => (
+  <div>
+    NavigationComponent Component
+  </div>
+);
+
+NavigationComponent.propTypes = {};
+
+NavigationComponent.defaultProps = {};
+
+export default NavigationComponent;
